58d60eb583438c71b6aeb65441697a3b362ed71e
PALAK SHUKLA <palakshuklaa0208@gmail.com>
2026-03-01 20:05:46 +0530

    23rd commit

---
+++ day23.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void append(Node** head, int data) {
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+Node* mergeLists(Node* l1, Node* l2) {
+    Node dummy;
+    Node* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 && l2) {
+        if (l1->data <= l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (l1) tail->next = l1;
+    if (l2) tail->next = l2;
+
+    return dummy.next;
+}
+
+void printList(Node* head) {
+    while (head) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, m, val;
+    Node *list1 = NULL, *list2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        append(&list1, val);
+    }
+
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &val);
+        append(&list2, val);
+    }
+
+    Node* merged = mergeLists(list1, list2);
+    printList(merged);
+
+    return 0;
+}
