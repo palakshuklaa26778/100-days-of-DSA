aeea5705c293255d7d6bec76a5fb7271e32ab4b0
PALAK SHUKLA <palakshuklaa0208@gmail.com>
2026-03-01 20:03:23 +0530

    19th commit
    Implement a program to find two numbers that sum closest to zero.

---
+++ day19.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int compare(const void* a, const void* b) {
+    int x = *(const int*)a;
+    int y = *(const int*)b;
+    return x - y;
+}
+
+int main() {
+    int n;
+    int arr[100];
+    
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    qsort(arr, n, sizeof(int), compare);
+    
+    int left = 0, right = n - 1;
+    int minSum = INT_MAX;
+    int resLeft = 0, resRight = 0;
+    
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        
+        if (abs(sum) < abs(minSum)) {
+            minSum = sum;
+            resLeft = arr[left];
+            resRight = arr[right];
+        }
+        
+        if (sum < 0) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    printf("%d %d\n", resLeft, resRight);
+    return 0;
+}
